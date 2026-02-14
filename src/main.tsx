2a2ab31abba4b5912e60ba271379217b055f030c
Paul2G <pggaleana@gmail.com>
2026-02-13 17:10:48 -0800

    feat: install and integrate tanstack router and query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import ReactDOM from 'react-dom/client';
 
 import '@/styles/index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <div>Hello</div>
-  </StrictMode>,
-);
+import { QueryClient } from '@tanstack/react-query';
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+const queryClient = new QueryClient();
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
+});
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
