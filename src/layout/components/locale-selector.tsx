75239b0e330b9f7a2ab5dd38f13a56161a4e752d
Paul2G <pggaleana@gmail.com>
2026-02-13 19:39:54 -0800

    feat: add locale and theme selectors

---
+++ src/layout/components/locale-selector.tsx
@@ -0,0 +1,59 @@
+import type { Locale } from '@/config/constants/locales';
+
+import { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { localeData, locales } from '@/config/constants/locales';
+import {
+  setLocaleInDocument,
+  setUserLocalePreference,
+} from '@/config/lib/locales';
+import { Button } from '@/core/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/core/components/ui/dropdown-menu';
+
+export function LocaleSelector() {
+  const { i18n } = useTranslation();
+  const [isOpen, setOpen] = useState(false);
+
+  const selectedLocale = useMemo(
+    () => locales.find((l) => localeData[l].langKey === i18n.language)!,
+    [i18n.language],
+  );
+
+  async function selectLocale(locale: Locale) {
+    await i18n.changeLanguage(locale);
+    setUserLocalePreference(locale);
+    setLocaleInDocument(locale);
+  }
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          {localeData[selectedLocale].langLabel}
+          <i className="ph ph-caret-down" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuRadioGroup
+          value={selectedLocale}
+          onValueChange={(value) =>
+            selectLocale(locales.find((locale) => locale === value)!)
+          }
+        >
+          {locales.map((l) => (
+            <DropdownMenuRadioItem value={l} key={l}>
+              {localeData[l].langLabel}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ src/layout/components/theme-selector.tsx
@@ -0,0 +1,45 @@
+import type { Theme } from '@/layout/contexts/theme-provider';
+
+import { MoonIcon, SunIcon } from '@phosphor-icons/react';
+import { useTranslation } from 'react-i18next';
+
+import { Button } from '@/core/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/core/components/ui/dropdown-menu';
+import { useTheme } from '@/layout/hooks/use-theme';
+
+const themes: Array<Theme> = ['light', 'dark', 'system'] as const;
+
+export function ThemeSelector() {
+  const { t } = useTranslation();
+  const { setTheme, theme: selectedTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">{t('actions.toggleTheme')}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuRadioGroup
+          value={selectedTheme}
+          onValueChange={(value) => setTheme(value as Theme)}
+        >
+          {themes.map((theme) => (
+            <DropdownMenuRadioItem key={theme} value={theme}>
+              {t(`themes.${theme}`)}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
