739d806fd37d8fdc9c0b7dce84bcb0ff27fbcc0e
Paul2G <pggaleana@gmail.com>
2026-02-21 18:34:58 -0800

    refactor(auth): create dummy logic to handle auth

--- src/modules/auth/contexts/auth-provider.tsx
+++ src/modules/auth/contexts/auth-provider.tsx
@@ -1,51 +1,56 @@
+import type { LoginData, User } from '@/modules/auth/types';
 import type { ReactNode } from 'react';
 
 import { createContext, useCallback, useEffect, useState } from 'react';
 
-import { sleep } from '@/core/lib/utils';
+import { checkAuth, login, logout } from '@/modules/auth/api';
+import { getUserToken, removeUserToken } from '@/modules/auth/lib/token';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext<AuthContextValue | null>(null);
 
-const key = 'tanstack.auth.user';
-
-function getStoredUser(): string {
-  return localStorage.getItem(key) ?? '';
-}
-
-function setStoredUser(user: string | null) {
-  if (user) {
-    localStorage.setItem(key, user);
-  } else {
-    localStorage.removeItem(key);
-  }
-}
-
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<string | null>(getStoredUser());
-  const isAuthenticated = !!user;
+  const [user, setUser] = useState<User | null>(null);
+  const isAuthenticated = Boolean(user);
 
-  const logout = useCallback(async () => {
-    await sleep(250);
-
-    setStoredUser(null);
-    setUser(null);
+  const handleLogin = useCallback(async (loginData: LoginData) => {
+    const user = await login(loginData);
+    setUser(user);
   }, []);
 
-  const login = useCallback(async (username: string) => {
-    await sleep(500);
+  const handleLogout = useCallback(async () => {
+    await logout();
+    removeUserToken();
+  }, []);
 
-    setStoredUser(username);
-    setUser(username);
+  const handleCheckAuth = useCallback(async () => {
+    const token = getUserToken();
+
+    if (token) {
+      try {
+        const user = await checkAuth(token);
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+        removeUserToken();
+      }
+    }
   }, []);
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    setUser(getStoredUser());
+    handleCheckAuth().then();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -53,9 +58,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
 export type AuthContextValue = {
   isAuthenticated: boolean;
-  login: (username: string) => Promise<void>;
+  login: (loginData: LoginData) => Promise<void>;
   logout: () => Promise<void>;
-  user: string | null;
+  user: User | null;
 };
 
 export type AuthProviderProps = {
--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -3,3 +3,10 @@ import type { loginSchema } from '@/modules/auth/schemas';
 import { z } from 'zod';
 
 export type LoginData = z.infer<typeof loginSchema>;
+
+export type User = {
+  id: string;
+  username: string;
+  email: string;
+  token: string;
+};
